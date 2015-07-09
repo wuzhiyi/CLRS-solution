9545b581d3aad74597915eacdac1d2d6f8dad670
vinci <1073143854@qq.com>
2015-07-09 10:51:56 +0800

    Create E_10.2-7.c

---
+++ Chapter10/E_10.2-7.c
@@ -0,0 +1,63 @@
+/**************************************************************
+ * Copyright (c) Wuzhiyi
+ * Introduction to Algorithms
+ *
+ * 题目:  Exercise 10.2-7
+ * 名称:  Reverse List
+ * 作者:  skanev
+ * 语言:  c
+ * 内容摘要: 非递归逆转单链表 
+ *
+ * 修改记录:
+ * 修改日期       版本号       修改人       修改内容
+ * ------------------------------------------------------------
+ * 20150709       V1.0          wuzhiyi      创建
+**************************************************************/
+#include <stdlib.h>
+
+typedef struct node_t {
+    int key;
+    struct node_t *next;
+} node_t;
+
+typedef struct {
+    struct node_t nil;
+} list_t;
+
+void init_list(list_t *list) {
+    list->nil.key = 0;
+    list->nil.next = &(list->nil);
+}
+
+void destroy_list(list_t *list) {
+    node_t *node = list->nil.next;
+    node_t *next;
+    
+    while (node != &(list->nil)) {
+        next = node->next;
+        free(node);
+        node = next;
+    }
+}
+
+void insert(list_t *list, int key) {
+    node_t *new = (node_t *) malloc(sizeof(node_t));
+    new->key = key;
+    new->next = list->nil.next;
+    list->nil.next = new;
+}
+
+void reverse(list_t *list) {
+    node_t *prev = &(list->nil);
+    node_t *node = list->nil.next;
+    node_t *next;
+    
+    while (node != &(list->nil)) {
+        next = node->next;
+        node->next = prev;
+        prev = node;
+        node = next;
+    }
+    
+    list->nil.next = prev;
+}
