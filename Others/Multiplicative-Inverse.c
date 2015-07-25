b5c41afb60777ab7470db5ffb3459a1c721f16e6
vinci <1073143854@qq.com>
2015-07-25 14:00:41 +0800

    Update Multiplicative-Inverse.c

--- Others/Multiplicative-Inverse.c
+++ Others/Multiplicative-Inverse.c
@@ -6,20 +6,22 @@
  * 名称:  乘法逆元
  * 作者:  wuzhiyi
  * 语言:  c
- * 内容摘要: 暴力法，费马小定理，（欧几里得扩展，神奇推倒）
+ * 内容摘要: 暴力法，费马小定理，扩展欧几里得算法，（预处理法，神奇推倒公式法）
  *
  * 修改记录:
  * 修改日期       版本号       修改人       修改内容
  * ------------------------------------------------------------
- * 20150724      V1.0        wuzhiyi     创建
+ * 20150724       V1.0         wuzhiyi      创建
+ * 20150725       V2.0         wuzhiyi      添加扩展欧几里得算法
  **************************************************************/
 #include <stdio.h>
 
 //初始定义 A*B = 1 mod M
-#define A   13
-#define M   23
+#define A   89
+#define M   191
 
 int Divide_Remainder (int, int ,int);
+int inv[M]; //Pre_Inv 变量数组声明
 
 //暴力求解逆元
 int Brute_Inv(int a, int m) {
@@ -52,9 +54,6 @@ int Prime(int a) {
             break;
         }
     }
-    if (result != 0) {
-        printf ("%d is prime\n", a);
-    }
     return result;
 }
 
@@ -85,8 +84,63 @@ int Divide_Remainder(int a, int b, int m) {
     return a%m;
 }
 
+//扩展欧几里得算法
+int Ext_Euclid_Gcd(int a, int b, int *x, int *y) {
+    if (b==0) { //若b=0,则 ax=gcd(a,0)=a
+        *x = 1, *y = 0;
+        return a;
+    }
+    else  {
+        if (Prime(A) && Prime(M)) { //A 与 M 互素, gcd(A,M)=1 时成立
+            int r = Ext_Euclid_Gcd(b, a%b, x, y);
+            int t = *x;
+            *x = *y;
+            *y = t - a/b * (*y);
+            if (*x < 0) *x = (*x%M + M)%M;  //如果 x 为负数，取模对称的正数
+            return r;
+        }
+        else {
+            printf ("A 与 MOD 不互素，不能使用扩展欧几里得定理\n");
+            return 0;
+        }
+    }
+}
+
+//预处理法和神奇推倒公式法都还没完善
+
+/* 预处理法
+ * 遍历所有 1~i 关于 M 逆元，因为逆元是积性函数，所以只要 M>i 成立即可，不需要 M 必须为素数
+ */
+void Pre_Inv() {
+    inv[0] = inv[1] = 1;
+    for (int i=2; i<M; i++) {
+        inv[i] = ((M-M/i)*inv[M%i])%M;
+    }
+}
+
+/* 神奇推倒公式法
+ * 以下是我的神奇推倒:
+ * 假设 i 的逆元为 Inv(i)
+ * 令
+ * M = x*i+r (0<i<M)
+ * x*i = r (mod M)
+ * x*i*Inv(r) = r*Inv(r) = 1 (mod M)
+ * 所以
+ * i 的逆元 Inv(i) = x*Inv(r)
+ */
+int Amazing_Inv(int a, int b) {
+    return (b-1)/a;
+}
+
 int main () {
+    int x, y;   //Ext_Euclid_Gcd 的变量初始化
     printf ("求乘法逆元: %d*x = 1 mod %d\n", A, M);
     printf ("暴力求乘法逆元:\tx = %d\n", Brute_Inv(A, M));
     printf ("费马小定理求逆元:\tx = %d\n", Fermat_Inv(A, M));
+    if (Ext_Euclid_Gcd(A, M, &x, &y)) {
+        printf ("扩展欧几里得求解：\n");
+        printf ("扩展欧几里得算法:\tx = %d\n", x);
+    }
+    printf ("预处理法求解：\n预处理法求逆元:\tx = %d\n", inv[A]);
+    printf ("我自己发明的神奇推倒法:\n神器推导法求逆元:\tx = %d", Amazing_Inv(A, M));
 }
